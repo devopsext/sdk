5b91deb55bb81657833e0a649c8fab2eee519369
Andrey Kolesnichenko <andrey.kolesnichenko@exness.com>
2022-01-31 19:11:23 +0200

    fix clickhouse panel json unmarshalling

--- panel.go
+++ panel.go
@@ -531,10 +531,13 @@ type Target struct {
 		Field string `json:"field"`
 		Type  string `json:"type"`
 	} `json:"metrics,omitempty"`
-	Query      string `json:"query,omitempty"`
-	Alias      string `json:"alias,omitempty"`
-	RawQuery   bool   `json:"rawQuery,omitempty"`
-	TimeField  string `json:"timeField,omitempty"`
+	Query string `json:"query,omitempty"`
+	Alias string `json:"alias,omitempty"`
+
+	// Doesn't work with Clickhouse
+	// RawQuery   bool   `json:"rawQuery,omitempty"`
+	RawQuery   interface{} `json:"rawQuery,omitempty"`
+	TimeField  string      `json:"timeField,omitempty"`
 	BucketAggs []struct {
 		ID       string `json:"id"`
 		Field    string `json:"field"`
